47dceceea9ee5c0c9150c12acfc07da0fc88b64d
cyril ferte <cyril@fertec.fr>
2016-08-07 23:09:37 +0000

    end v1

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,4 +1,5 @@
 class Gif < ApplicationRecord
   validates :url, presence: true
-  validates :tag, presence: true
+  validates :category, inclusion: { in: %w(anal BBW bukkake lesbian mature romantic yong hard),
+    message: "%{value} is not a valid category" }
 end
