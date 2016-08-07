31f145d98da4064fe57c70dcfbf32ddd3c8e5258
cyril ferte <cyril@fertec.fr>
2016-08-07 23:23:55 +0000

    heruku deploy2

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,5 +1,5 @@
 class Gif < ApplicationRecord
   validates :url, presence: true
-  validates :category, inclusion: { in: %w(anal BBW bukkake lesbian mature romantic yong hard),
+  validates :tag, inclusion: { in: %w(anal BBW bukkake lesbian mature romantic yong hard),
     message: "%{value} is not a valid category" }
 end
